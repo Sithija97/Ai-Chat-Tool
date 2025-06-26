6459cfbfacb9f921b2b357b6cf1c4427c3516bd7
Sithija Shehara <nsithijashehara@gmail.com>
2025-06-26 21:55:07 +0530

    util functions

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,26 +1,35 @@
 import { STATUS } from "@/enums";
 import type { FileType } from "@/molecules";
-import { GoogleGenAI } from "@google/genai";
+import type { GoogleGenAI } from "@google/genai";
 import type React from "react";
+import { Buffer } from "buffer";
 
 type IProps = {
+  ai: GoogleGenAI;
   file: FileType;
+  input?: string;
+  messages?: {
+    role: string;
+    text: string;
+  }[];
+  setMessages?: React.Dispatch<
+    React.SetStateAction<
+      {
+        role: string;
+        text: string;
+      }[]
+    >
+  >;
   setStatus: React.Dispatch<React.SetStateAction<string>>;
-  setSummary: React.Dispatch<React.SetStateAction<string>>;
+  setSummary?: React.Dispatch<React.SetStateAction<string>>;
 };
 
 export const generateSumary = async ({
+  ai,
   file,
   setStatus,
   setSummary,
 }: IProps) => {
-  console.log({
-    file,
-    setStatus,
-    setSummary,
-  });
-  const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_GEMINI_API });
-
   const contents = [
     {
       text: "Summarize this document, but it should be an advanced summary about the given subject",
@@ -39,7 +48,7 @@ export const generateSumary = async ({
       model: "gemini-2.0-flash",
       contents: contents,
     });
-    if (response && typeof response.text === "string") {
+    if (response && typeof response.text === "string" && setSummary) {
       setStatus(STATUS.SUCCESS);
       setSummary(response.text);
       console.log(response.text);
@@ -51,4 +60,68 @@ export const generateSumary = async ({
     setStatus(STATUS.IDLE);
     console.log("Summary generation completed.");
   }
+};
+
+export const generateCoversation = async ({
+  ai,
+  file,
+  input,
+  messages,
+  setMessages,
+  setStatus,
+}: IProps) => {
+  const contents = [
+    {
+      text: `Answeer the following question based on the provided document: ${input}.
+      Answer with a single or two line and be concise and text only. (no markdwns, tags or symbols).
+      Chat history: ${JSON.stringify(messages)}`,
+    },
+    {
+      inlineData: {
+        mimeType: file.type,
+        data: file.file,
+      },
+    },
+  ];
+
+  setStatus(STATUS.LOADING);
+  try {
+    const response = await ai.models.generateContent({
+      model: "gemini-2.0-flash",
+      contents: contents,
+    });
+    if (response && typeof response.text === "string") {
+      setStatus(STATUS.SUCCESS);
+      console.log(response.text);
+      if (messages && setMessages) {
+        setMessages([...messages, { role: "model", text: response.text }]);
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    setStatus(STATUS.ERROR);
+    if (messages && setMessages) {
+      setMessages([
+        ...messages,
+        { role: "error", text: "Error sending messages, Please try again." },
+      ]);
+    }
+  } finally {
+    setStatus(STATUS.IDLE);
+    console.log("Conversation generation completed.");
+  }
+};
+
+export const generateFile = async (fileObj: File) => {
+  const fileUpload = await fileObj.arrayBuffer();
+
+  const file: FileType = {
+    name: fileObj.name,
+    type: fileObj.type,
+    file: Buffer.from(fileUpload).toString("base64"),
+    imageUrl: fileObj.type.includes("pdf")
+      ? "/pdf-icon.png"
+      : URL.createObjectURL(fileObj),
+  };
+  return file;
 };
