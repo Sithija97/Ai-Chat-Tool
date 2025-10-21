09ef13454283332478befd7a566d453984319c8a
Sithija Shehara <nsithijashehara@gmail.com>
2025-10-21 18:34:14 +0530

    removed unused code

--- client/src/templates/main.template.tsx
+++ client/src/templates/main.template.tsx
@@ -3,11 +3,6 @@ import { ChatWindow } from "@/organisms";
 export const MainTemplate = () => {
   return (
     <main className="container">
-      {/* <Header /> */}
-      {/* {uploadFile ? (
-        <Summary file={uploadFile} />
-      ) : (
-        )} */}
       <ChatWindow />
     </main>
   );
